ce095370c64d3258dfb868a78c47dcc32e6c7b19
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2023-11-13 18:54:10 +0000

    sprite only changes frames now

--- src/sprite.js
+++ src/sprite.js
@@ -1,88 +1,34 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-function SpriteAnimation() {
+const SpriteAnimator = () => {
   const canvasRef = useRef(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const moveSpeed = 4; // The speed of the sprite
-  const spriteW = 48; // The width of your sprite frame
-  const spriteH = 60; // The height of your sprite frame
+  const spriteW = 48;
+  const spriteH = 60;
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
     const img = new Image();
-    img.src = 'player.png'; // Replace with the path to your sprite image
-    
-    let moveDirection = 'right'; // Initial movement direction
+    img.src = 'player.png'; // Make sure this path is correct in your project
+    let currentCycle = 0;
 
     img.onload = () => {
-      // Initial draw
-      context.drawImage(img, 0, 0, spriteW, spriteH, position.x, position.y, spriteW, spriteH);
+      const interval = setInterval(() => {
+        const canvas = canvasRef.current;
+        const cx = canvas.getContext('2d');
+        cx.clearRect(0, 0, spriteW, spriteH);
+        cx.drawImage(img,
+          // source rectangle
+          currentCycle * spriteW, 0, spriteW, spriteH,
+          // destination rectangle
+          0, 0, spriteW, spriteH
+        );
+        currentCycle = (currentCycle + 1) % 8;
+      }, 300);
+
+      return () => clearInterval(interval);
     };
+  }, []); // Removed cycle from dependency array
 
-    // Function to update the sprite's position
-    const updatePosition = () => {
-      setPosition((prev) => {
-        let newX = prev.x;
-        let newY = prev.y;
+  return <canvas ref={canvasRef} width={spriteW} height={spriteH} />;
+};
 
-        // Update the position based on the direction
-        switch (moveDirection) {
-          case 'right':
-            newX += moveSpeed;
-            if (newX >= canvas.width - spriteW) { // Change direction when reaching the edge
-              moveDirection = 'down';
-            }
-            break;
-          case 'down':
-            newY += moveSpeed;
-            if (newY >= canvas.height - spriteH) {
-              moveDirection = 'left';
-            }
-            break;
-          case 'left':
-            newX -= moveSpeed;
-            if (newX <= 0) {
-              moveDirection = 'up';
-            }
-            break;
-          case 'up':
-            newY -= moveSpeed;
-            if (newY <= 0) {
-              moveDirection = 'right';
-            }
-            break;
-          default:
-            break;
-        }
-        return { x: newX, y: newY };
-      });
-    };
-
-    // Set an interval to move the sprite every 50ms
-    const intervalId = setInterval(updatePosition, 50);
-
-    // Clear the interval when the component unmounts
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  // Effect to redraw the sprite whenever its position changes
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    const img = new Image();
-    img.src = 'player.png'; // Replace with the path to your sprite image
-
-    img.onload = () => {
-      // The draw function needs to be called here again to update the sprite's position
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(img, 0, 0, spriteW, spriteH, position.x, position.y, spriteW, spriteH);
-    };
-  }, [position]); // Only re-run the effect if the position changes
-
-  return <canvas ref={canvasRef} width="600" height="400" />;
-}
-
-export default SpriteAnimation;
+export default SpriteAnimator;
