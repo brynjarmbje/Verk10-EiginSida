97151f73e64a18383bb3d6cd494bd5758f4d016e
brynjarmbje <129754325+brynjarmbje@users.noreply.github.com>
2023-11-13 18:44:52 +0000

    Breyta sprite virkni

--- src/sprite.js
+++ src/sprite.js
@@ -3,70 +3,83 @@ import React, { useRef, useEffect, useState } from 'react';
 function SpriteAnimation() {
   const canvasRef = useRef(null);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const moveSpeed = 4; // The speed of the sprite
   const spriteW = 48; // The width of your sprite frame
   const spriteH = 60; // The height of your sprite frame
-  const moveSpeed = 5; // How many pixels the sprite moves per arrow key press
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
     const img = new Image();
-    img.src = 'player.png'; // Make sure 'player.png' is the correct path to your sprite sheet
+    img.src = 'player.png'; // Replace with the path to your sprite image
+    
+    let moveDirection = 'right'; // Initial movement direction
 
     img.onload = () => {
-      // Draw the image to the canvas once it's loaded
+      // Initial draw
       context.drawImage(img, 0, 0, spriteW, spriteH, position.x, position.y, spriteW, spriteH);
     };
 
-    // Function to update the canvas whenever the position state changes
-    const draw = () => {
-      context.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-      context.drawImage(img, 0, 0, spriteW, spriteH, position.x, position.y, spriteW, spriteH);
+    // Function to update the sprite's position
+    const updatePosition = () => {
+      setPosition((prev) => {
+        let newX = prev.x;
+        let newY = prev.y;
+
+        // Update the position based on the direction
+        switch (moveDirection) {
+          case 'right':
+            newX += moveSpeed;
+            if (newX >= canvas.width - spriteW) { // Change direction when reaching the edge
+              moveDirection = 'down';
+            }
+            break;
+          case 'down':
+            newY += moveSpeed;
+            if (newY >= canvas.height - spriteH) {
+              moveDirection = 'left';
+            }
+            break;
+          case 'left':
+            newX -= moveSpeed;
+            if (newX <= 0) {
+              moveDirection = 'up';
+            }
+            break;
+          case 'up':
+            newY -= moveSpeed;
+            if (newY <= 0) {
+              moveDirection = 'right';
+            }
+            break;
+          default:
+            break;
+        }
+        return { x: newX, y: newY };
+      });
     };
 
-    // This function is triggered by the keydown event
-    const handleKeyDown = (e) => {
-      e.preventDefault();
-      switch (e.key) {
-        case '37':
-          setPosition((prev) => ({ ...prev, x: prev.x - moveSpeed }));
-          break;
-        case 39:
-          setPosition((prev) => ({ ...prev, x: prev.x + moveSpeed }));
-          break;
-        case 'ArrowUp':
-          setPosition((prev) => ({ ...prev, y: prev.y - moveSpeed }));
-          break;
-        case 'ArrowDown':
-          setPosition((prev) => ({ ...prev, y: prev.y + moveSpeed }));
-          break;
-        default:
-          break;
-      }
-    };
+    // Set an interval to move the sprite every 50ms
+    const intervalId = setInterval(updatePosition, 50);
 
-    // Attach the event listener for the arrow keys
-    window.addEventListener('keydown', handleKeyDown);
-
-    // Remove the event listener when the component unmounts
+    // Clear the interval when the component unmounts
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
+      clearInterval(intervalId);
     };
   }, []);
 
-  // Effect to re-draw the sprite whenever its position changes
+  // Effect to redraw the sprite whenever its position changes
   useEffect(() => {
-    if (canvasRef.current) {
-      const context = canvasRef.current.getContext('2d');
-      const img = new Image();
-      img.src = 'player.png'; // Your sprite image path
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    img.src = 'player.png'; // Replace with the path to your sprite image
 
-      img.onload = () => {
-        // The draw function needs to be called here again to update the sprite's position
-        context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-        context.drawImage(img, 0, 0, spriteW, spriteH, position.x, position.y, spriteW, spriteH);
-      };
-    }
+    img.onload = () => {
+      // The draw function needs to be called here again to update the sprite's position
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(img, 0, 0, spriteW, spriteH, position.x, position.y, spriteW, spriteH);
+    };
   }, [position]); // Only re-run the effect if the position changes
 
   return <canvas ref={canvasRef} width="600" height="400" />;
